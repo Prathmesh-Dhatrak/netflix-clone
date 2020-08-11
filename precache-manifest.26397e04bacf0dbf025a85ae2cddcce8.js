1a61c28e4a0846f6836187dd7dede2d46cf79455
Prathmesh Santosh Dhatrak <prathmesh101dhatrak@gmail.com>
2020-08-11 19:16:32 +0530

    Updates

---
+++ precache-manifest.26397e04bacf0dbf025a85ae2cddcce8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "820443ee54b165050ae4beef85ca015d",
+    "url": "/github.io/netflix-clone/index.html"
+  },
+  {
+    "revision": "d2e473d2f915d50ffd5b",
+    "url": "/github.io/netflix-clone/static/css/main.0dda8d65.chunk.css"
+  },
+  {
+    "revision": "2f433471b27a049116bf",
+    "url": "/github.io/netflix-clone/static/js/2.7eca4d64.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/github.io/netflix-clone/static/js/2.7eca4d64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2e473d2f915d50ffd5b",
+    "url": "/github.io/netflix-clone/static/js/main.fbdab90a.chunk.js"
+  },
+  {
+    "revision": "e45c6dbad7e105eb77d6",
+    "url": "/github.io/netflix-clone/static/js/runtime-main.f32cdbe2.js"
+  }
+]);
